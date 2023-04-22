1c5d8d896f42cc2650e2543d70993899037fed67
jjdavy225 <jeanjauresdavy@gmail.com>
2023-04-22 19:55:53 +0000

    Welcome image changed

--- resources/views/galaWelcome.blade.php
+++ resources/views/galaWelcome.blade.php
@@ -17,17 +17,17 @@
 <!-- particles.js container -->
 <div id="particles-js" class="h-screen " >
     <div class="flex flex-col items-center justify-center w-full min-h-screen font-bold text-center " style="position:fixed">
-        <img src="{{asset('img/gala ed.png')}}" class="w-32 md:w-64 md:h-64 h-32" alt="Logo Gala">
+        <img src="{{asset('img/logo_acceuil.png')}}" class="w-32 md:w-64 md:h-64 h-32" alt="Logo Gala">
         {{-- <span class="text-white text-7xl" style="font-size:33px">
            IT GALA 2022
         </span>    --}}
         <span class="text-2xl text-gray-200" >
             Bienvenue sur l'application de vote des IT AWARDS.
-        </span>   
+        </span>
         <br>
 
         @if (Route::has('login'))
-       
+
             @auth
                 <a  href="{{ url('/dashboard') }}" class="flex pt-2 text-xl text-white px-4 py-2 text-myblue bg-white border border-white rounded-md shadow-md" href="">
                     Mon espace de vote
@@ -38,11 +38,11 @@
                 <a  href="{{route('login') }}" class="flex pt-2 text-xl text-white px-4 py-2 text-myblue bg-white border border-white rounded-md shadow-md" href="">
                     Se connecter
                 </a>
-                
+
             @endauth
-        
+
         @endif
-        
+
 
     </div>
 </div>
